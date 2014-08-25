ab9f78cf356f70387c7428ffa2820924e5d1a9a2
JeremiahOriendo <jeremiah.oriendo@gmail.com>
2014-08-25 17:55:36 +0800

    change the hearts to red

--- SP1Framework/Donovan.cpp
+++ SP1Framework/Donovan.cpp
@@ -7,9 +7,12 @@ void LifeCounter()
 {
 	gotoXY(60,0);
 	colour(0x10A);
+	std::cout << "Life:";
 	for( int x = 0; x < Life ; x++)
+	{
+		colour(0x0C);
 		std::cout << char(3);
-		
+	}	
 	if (Life==0)
 	{
 		GameOver();
